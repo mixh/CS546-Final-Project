ca2a7efad574a0bb84768f79d2b29226528f3387
Sarthak Vaidya <savaidya15@gmail.com>
2023-04-04 17:28:10 -0400

    checkString and checkStringArray methods added to validation.js

---
+++ validation.js
@@ -0,0 +1,27 @@
+const exportedMethods = {
+    checkString(strVal, varName) {
+      if (!strVal) throw `Error: You must supply a ${varName}!`;
+      if (typeof strVal !== 'string') throw `Error: ${varName} must be a string!`;
+      strVal = strVal.trim();
+      if (strVal.length === 0)
+        throw `Error: ${varName} cannot be an empty string or string with just spaces`;
+      if (!isNaN(strVal))
+        throw `Error: ${strVal} is not a valid value for ${varName} as it only contains digits`;
+      return strVal;
+    },
+  
+    checkStringArray(arr, varName) {
+      if (!arr || !Array.isArray(arr))
+        throw `You must provide an array of ${varName}`;
+      for (let i in arr) {
+        if (typeof arr[i] !== 'string' || arr[i].trim().length === 0) {
+          throw `One or more elements in ${varName} array is not a string or is an empty string`;
+        }
+        arr[i] = arr[i].trim();
+      }
+  
+      return arr;
+    }
+  };
+  
+  export default exportedMethods;
