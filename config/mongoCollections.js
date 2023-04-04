5405094649f92d6e2841d5ae4303face30ee1376
Sarthak Vaidya <savaidya15@gmail.com>
2023-04-04 17:08:18 -0400

    Users collection created

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -1,1 +1,17 @@
 //add collections for the database
+import { dbConnection } from './mongoConnection.js';
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+export const users = getCollectionFn('users');
