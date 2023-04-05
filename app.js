afa2161a6dd54211a3b231d7b222f2d49d3abc51
Mihir <mihirxjain@gmail.com>
2023-04-05 18:40:05 -0400

    updated app.js for express-session and bcrypt

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const __dirname = dirname(__filename);
 
 const staticDir = express.static(__dirname + "/public");
 
+import session from 'express-session';
+
 const rewriteUnsupportedBrowserMethods = (req, res, next) => {
   // If the user posts to the server with a property called _method, rewrite the request's method
   // To be that method; so if they post _method=PUT you can now allow browsers to POST to a route that gets
@@ -28,6 +30,17 @@ app.use("/public", staticDir);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(rewriteUnsupportedBrowserMethods);
+
+// configure express-session middleware
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
 
 app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
