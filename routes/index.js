9ba81eae11cec5e531bcc4f91dcaf374f7fb227f
Mihir Jain <63667062+mixh@users.noreply.github.com>
2023-04-17 16:03:20 -0400

    intial matches routing brain fart

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@
 
 import initRoutes from "./init.js";
 import profileRoutes from "./profile.js"
+import matchesRoutes from "./matches.js";
 
 const constructorMethods = (app) => {
   app.use("/", initRoutes);
   app.use("/profile", profileRoutes);
+  app.use("/matches", matchesRoutes);
   app.use("*", (req, res) => {
     res.redirect("/");
   });
