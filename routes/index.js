6837d5cb0692cb5d13587adf2e62fbfe39a075ed
Mihir Jain <63667062+mixh@users.noreply.github.com>
2023-04-20 18:02:23 -0400

    Revert "Dev mihir to Main/DEV"

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
-import authRoutes from "./auth.js";
-import homeRoutes from "./home.js";
-import profileRoutes from "./profile.js";
-import potentialMatchesRoutes from "./potentialMatches.js"
+//import and export all
+
+import initRoutes from "./init.js";
+import profileRoutes from "./profile.js"
 
 const constructorMethods = (app) => {
-  app.use("/", authRoutes);
-  app.use("/home",homeRoutes);
+  app.use("/", initRoutes);
   app.use("/profile", profileRoutes);
-  app.use("/potentialMatches",potentialMatchesRoutes);
   app.use("*", (req, res) => {
     res.redirect("/");
   });
