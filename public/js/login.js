f51dc73382e0a7f533efa7ea2aff7d2bbe22b880
Yash Baleri <baleri.yash@gmail.com>
2023-04-28 00:43:27 -0400

    login.js error handling

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,35 @@ const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 let errorDiv = document.getElementById('error');
 
+function checkEmail(email, varName){
+  if(typeof email !== 'string'){
+      throw `${varName} must be a string`;
+  }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if(!email.match(emailRegex)){
+     throw `The ${varName} must be a valid email address`;
+  }
+}
+
+function checkPassword(password,varName){
+  if(typeof password !== 'string'){
+    throw `${varName} must be of string type`;
+}
+
+if (password.length < 8) {
+ throw 'Password must be at least 8 characters long';
+}
+if (!/[a-z]/.test(password)) {
+ throw 'Password must contain at least one lowercase letter';
+}
+if (!/[A-Z]/.test(password)) {
+ throw 'Password must contain at least one uppercase letter';
+}
+if (!/\d/.test(password)) {
+ throw 'Password must contain at least one number';
+}
+}
+
 
 loginForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -22,6 +51,14 @@ loginForm.addEventListener('submit', (event) => {
       errorDiv.hidden = false;
       errorDiv.innerHTML = 'You must enter a password';
       passwordInput.focus();
+      return;
+    }
+    try {
+      checkEmail(emailInput, "Email");
+      checkPassword(passwordInput, "Password");;
+    } catch (error) {
+      errorDiv.hidden = false;
+      errorDiv.innerHTML = error;
       return;
     }
     
