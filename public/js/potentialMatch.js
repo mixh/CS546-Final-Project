6d8d36daa77ad10247c2c7481e1f2f89f6d32d31
Sarthak Vaidya <savaidya15@gmail.com>
2023-04-30 14:58:48 -0400

    logic added to potentialMatch.js

---
+++ public/js/potentialMatch.js
@@ -0,0 +1,20 @@
+const url = new URL(window.location.href);
+const distance = url.searchParams.get("distance");
+
+if (distance === "all") {
+  document.getElementById("showAllProfilesButton").style.display = "none";
+} else {
+  document.getElementById("showWithin5kmButton").style.display = "none";
+}
+
+function showAllProfiles() {
+  const url = new URL(window.location.href);
+  url.searchParams.set("distance", "all");
+  window.location.href = url.toString();
+}
+
+function showProfilesWithin5km() {
+  const url = new URL(window.location.href);
+  url.searchParams.set("distance", "5km");
+  window.location.href = url.toString();
+}
